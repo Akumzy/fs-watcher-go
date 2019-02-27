3424e8bbba6d1d9e0f52132d4664f0a13b0ae7ce
Akuma Isaac Akuma <Akumanewton@gmail.com>
2019-02-27 18:29:57 +0100

    updated binary name

--- script.sh
+++ script.sh
@@ -2,7 +2,7 @@
 
 rm -Rf ./bin
 
-gox -osarch="linux/amd64" -osarch="linux/386" -osarch="darwin/amd64" -osarch="darwin/386" -osarch="windows/amd64" -osarch="windows/386" -output="./bin/{{.Dir}}_{{.OS}}_{{.Arch}}"
+gox -osarch="linux/amd64" -osarch="linux/386" -osarch="darwin/amd64" -osarch="darwin/386" -osarch="windows/amd64" -osarch="windows/386" -output="./bin/fs-watcher_{{.OS}}_{{.Arch}}"
 
 node script.js
 rm -Rf /home/akumzy/projects/watcher/bin
